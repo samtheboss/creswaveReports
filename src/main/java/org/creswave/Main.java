e59e629b5233eefeb93bc54dcc588ab4fd79a950
SmartApps <sammykagichu@gmail.com>
2024-06-14 15:48:10 +0300

    updated

--- src/main/java/org/creswave/Main.java
+++ src/main/java/org/creswave/Main.java
@@ -20,8 +20,8 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Loading .......");
- //stockMovementReport();
-   RegisterMOH420();
+ stockMovementReport();
+  // RegisterMOH420();
    // SpeechLanguageTherapyRegisterMOH418();
   }
 
